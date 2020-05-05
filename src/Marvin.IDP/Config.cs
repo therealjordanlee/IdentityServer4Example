961af4419d71c01601ed759aa9afac6a92399a0c
Jordan Lee <jordan@jordanlee.net>
2020-05-06 08:56:36 +1000

    first

---
+++ src/Marvin.IDP/Config.cs
@@ -0,0 +1,61 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = new Guid().ToString(), // SubjectId must be unique for each user
+                    Username = "jordan",
+                    Password = "password",
+                    Claims = new List<Claim> // Claims represent information about user
+                    {
+                        new Claim("given_name", "Jordan"),
+                        new Claim("family_name","Lee")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = new Guid().ToString(),
+                    Username = "max",
+                    Password = "password",
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Max"),
+                        new Claim("family_name","Payne")
+                    }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Identity resources map to scopes which give Identity related information
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+                {
+                    new IdentityResources.OpenId(), // Subject claim is returned
+                    new IdentityResources.Profile() // Profile related claims (e.g. given_name, family_name)
+                };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
---
+++ src/Marvin.IDP/Program.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Marvin.IDP
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
